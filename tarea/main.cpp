b3c35bfa98e5b3d110956038a9331c760252151a
mauriciosalazarsh <85299691+mauriciosalazarsh@users.noreply.github.com>
2023-04-09 21:48:40 -0500

    Update main.cpp

--- tarea/main.cpp
+++ tarea/main.cpp
@@ -8,6 +8,44 @@
 #include "foo.h"
 
 // Puede validar su codigo con la siguiente funcion main
-int main(){
+int main() {
+    srand(static_cast<unsigned>(time(0))); // Inicializa la semilla del generador de números aleatorios
+    Matriz2D m1;
+    Matriz2D m2(5);
+    Matriz2D m3(5, 2);
+    Matriz2D m4 = m2;
+    Matriz2D m5 = move(m2);
 
+    Matriz2D m6 = t(m3);
+
+    cout << "m1:" << endl << m1 << endl;
+    cout << "m2:" << endl << m2 << endl; // m2 debería estar vacío después del movimiento
+    cout << "m3:" << endl << m3 << endl;
+    cout << "m4:" << endl << m4 << endl;
+    cout << "m5:" << endl << m5 << endl;
+    cout << "m6 (transpuesta de m3):" << endl << m6 << endl;
+
+    Matriz2D m7 = m1 + 3;
+    Matriz2D m8 = m1 - 1.2;
+    Matriz2D m9 = m1 * 5;
+    Matriz2D m10 = m1 / 7;
+
+    cout << "m1 + 3:" << endl << m7 << endl;
+    cout << "m1 - 1.2:" << endl << m8 << endl;
+    cout << "m1 * 5:" << endl << m9 << endl;
+    cout << "m1 / 7:" << endl << m10 << endl;
+
+    // Asegúrate de que las dimensiones de m11 y m12 sean compatibles antes de probar las operaciones de suma, resta y multiplicación.
+    Matriz2D m11(3, 3);
+    Matriz2D m12(3, 3);
+
+    Matriz2D m13 = m11 + m12;
+    Matriz2D m14 = m11 - m12;
+    Matriz2D m15 = m11 * m12;
+
+    cout << "m11 + m12:" << endl << m13 << endl;
+    cout << "m11 - m12:" << endl << m14 << endl;
+    cout << "m11 * m12:" << endl << m15 << endl;
+
+    return 0;
 }
