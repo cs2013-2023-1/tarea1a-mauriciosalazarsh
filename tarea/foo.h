5f8d07e1c7529a52e1330b6a62aeecadefc151e0
mauriciosalazarsh <85299691+mauriciosalazarsh@users.noreply.github.com>
2023-04-09 21:48:20 -0500

    Update foo.h

--- tarea/foo.h
+++ tarea/foo.h
@@ -3,15 +3,15 @@
 using namespace std;
 
 class Matriz2D{
-friend Matriz2D t(Matriz2D&);
-friend ostream& operator<<(ostream&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator-(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator*(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, float);
-friend Matriz2D operator-(const Matriz2D&, float);
-friend Matriz2D operator*(const Matriz2D&, float);
-friend Matriz2D operator/(const Matriz2D&, float);
+    friend Matriz2D t(Matriz2D&);
+    friend ostream& operator<<(ostream&, const Matriz2D&);
+    friend Matriz2D operator+(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator-(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator*(const Matriz2D&, const Matriz2D&);
+    friend Matriz2D operator+(const Matriz2D&, float);
+    friend Matriz2D operator-(const Matriz2D&, float);
+    friend Matriz2D operator*(const Matriz2D&, float);
+    friend Matriz2D operator/(const Matriz2D&, float);
 public:
     Matriz2D();
     Matriz2D(int);
@@ -27,4 +27,5 @@ private:
     int filas;
     int columnas;
 };
+
 
